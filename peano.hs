9aae9ba550f4209bdb930b4aaa8dc86b133e407e
Alex Garella <agarella@gmail.com>
2014-07-01 21:08:29 +0200

    added multiply to peano

--- peano.hs
+++ peano.hs
@@ -9,4 +9,13 @@ peanoToNum Zero = 0
 peanoToNum (Succ x) = peanoToNum x + 1
 
 numToPeano 0 = Zero
-numToPeano n = Succ (numToPeano (n - 1)) 
+numToPeano n = Succ (numToPeano (n - 1))
+
+multiply x Zero 	= Zero
+multiply Zero y 	= Zero
+multiply (Succ Zero) y	= Succ Zero
+multiply x (Succ Zero)	= Succ Zero
+multiply x y 		= calcMultiply x x Zero
+
+calcMultiply x Zero z 		= z
+calcMultiply x (Succ y) z	= calcMultiply x y (add x z) 
