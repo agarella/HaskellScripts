5567520f35de09d0c97b768eb751900dc6c2ec2b
Alex Garella <Alexander.Garella@ing.nl>
2014-07-05 12:39:41 +0200

    added non tail-recursive fibonacci and factorial functions for comparison reasons

--- factorial.hs
+++ factorial.hs
@@ -2,3 +2,6 @@ factorial 0 = 1
 factorial n = factorialAux n 1
 		where	factorialAux 0 acc = acc
 			factorialAux n acc = factorialAux (n - 1) (n * acc)
+
+factorialRec 0 = 1
+factorialRec n = factorialRec (n - 1) * n
--- fibonacci.hs
+++ fibonacci.hs
@@ -3,3 +3,7 @@ fibonacci 1 = 1
 fibonacci n = fibonacciAux n 1 1
 		where	fibonacciAux 1 n1 n2	= n1
 			fibonacciAux n n1 n2	= fibonacciAux (n - 1) (n1 + n2) n1
+
+fibonacciRec 0 = 1
+fibonacciRec 1 = 1
+fibonacciRec n = fibonacciRec (n - 1) + fibonacciRec (n - 2)
