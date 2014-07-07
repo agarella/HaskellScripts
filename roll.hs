956572f1ff923b8aa690b4ea21cb50f58c6343ff
Alex Garella <Alexander.Garella@ing.nl>
2014-07-07 15:28:25 +0200

    role with negative integer does the operation to the opposite side

--- roll.hs
+++ roll.hs
@@ -1,3 +1,4 @@
 roll xs 0 = xs
 roll [] n = []
-roll (x:xs) n = roll (xs ++ [x]) (n - 1)
+roll (x:xs) n 	| n > 0 = roll (xs ++ [x]) (n - 1)
+		| otherwise = roll (last xs : reverse (drop 1 (reverse (x:xs)))) (n + 1)
