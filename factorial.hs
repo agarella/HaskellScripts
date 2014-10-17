71fcd3852ddcafcce5eb835554b2c0aa14d5bb0a
Alex Garella <agarella@gmail.com>
2014-10-17 18:40:19 +0200

    added type signatures

--- factorial.hs
+++ factorial.hs
@@ -1,7 +1,9 @@
+factorial :: (Num a, Eq a) => a -> a
 factorial 0 = 1
 factorial n = factorialAux n 1
-		where	factorialAux 0 acc = acc
-			factorialAux n acc = factorialAux (n - 1) (n * acc)
+ where factorialAux 0 acc = acc
+       factorialAux n acc = factorialAux (n - 1) (n * acc)
 
+factorialRec :: (Num a, Eq a) => a -> a
 factorialRec 0 = 1
 factorialRec n = factorialRec (n - 1) * n
