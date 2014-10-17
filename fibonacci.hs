989f2b51921747a2509db03741107f5a9c34023b
Alex <agarella@gmail.com>
2014-10-17 07:56:23 +0200

    added reversed fibonacciList and removed tabbing

--- fibonacci.hs
+++ fibonacci.hs
@@ -1,13 +1,18 @@
 fibonacci 0 = 1
 fibonacci n = fibonacciAux n 1 1
-		where	fibonacciAux 1 n1 n2	= n1
-			fibonacciAux n n1 n2	= fibonacciAux (n - 1) (n1 + n2) n1
+ where fibonacciAux 1 n1 n2 = n1
+       fibonacciAux n n1 n2 = fibonacciAux (n - 1) (n1 + n2) n1
 
 fibonacciRec 0 = 1
 fibonacciRec 1 = 1
 fibonacciRec n = fibonacciRec (n - 1) + fibonacciRec (n - 2)
 
 fibonacciList n | n < 0 = error "fibonacci n < 0 undefined"
-		| n == 0 = [1]
-		| n == 1 = 1 : fibonacciList (n - 1)
-		| otherwise = (sum.take 2) (fibonacciList (n - 1)) : fibonacciList (n - 1)
+                | n == 0 = [1]
+                | n == 1 = 1 : fibonacciList (n - 1)
+                | otherwise = (sum.take 2) (fibonacciList (n - 1)) : fibonacciList (n - 1)
+
+fibonacciListR n | n < 0 = error "fibnoacci n < 0 undefined"
+                 | n == 0 = [1]
+                 | n == 1 = fibonacciListR (n - 1) ++ [1]
+                 | otherwise = fibonacciListR (n - 1) ++ [sum (drop (n - 2) (fibonacciListR (n - 1)))]
