4722bb0370b70b49bbd66323aa5fb5bb25443508
Alex Garella <agarella@gmail.com>
2015-01-31 23:38:46 +0100

    Make BTree Foldable and implement contains

--- trees.hs
+++ trees.hs
@@ -1,12 +1,19 @@
+import Prelude hiding (foldr, or)
+import Data.Foldable
+
 ------------------------------------------------------------------------------------------------------------------------------
 -- Binary Tree
 ------------------------------------------------------------------------------------------------------------------------------
 
 data BTree a = Leaf a | Node a (BTree a) (BTree a) deriving (Show)  
 
-root (Leaf x) = x
-root (Node x y z) = x
+contains :: (Foldable t, Functor t, Eq a) => t a -> a -> Bool
+contains tree x = or $ fmap (== x) tree
 
 instance Functor BTree where
   fmap f (Leaf x) = Leaf (f x)
   fmap f (Node x y z) = Node (f x) (fmap f y) (fmap f z)
+
+instance Foldable BTree where
+  foldr f acc (Leaf x) = f x acc
+  foldr f acc (Node x y z) = foldr f (f x (foldr f acc z)) y
