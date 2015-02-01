eb9d9fdff39db7dba1321ab9f7af5ad9a8d6a877
Alex Garella <agarella@gmail.com>
2015-02-01 11:49:58 +0100

    Refactor

--- trees.hs
+++ trees.hs
@@ -1,19 +1,28 @@
 import Prelude hiding (foldr, or)
 import Data.Foldable
+import Data.Monoid
 
 ------------------------------------------------------------------------------------------------------------------------------
 -- Binary Tree
 ------------------------------------------------------------------------------------------------------------------------------
+data BTree a = Empty | Node a (BTree a) (BTree a) deriving (Show, Eq)
 
-data BTree a = Leaf a | Node a (BTree a) (BTree a) deriving (Show, Eq)  
+root :: BTree t -> t
+root Empty = error "root Empty"
+root (Node x _ _) = x
 
 contains :: (Foldable t, Functor t, Eq a) => t a -> a -> Bool
 contains tree x = or $ fmap (== x) tree
 
+findMax :: Ord b => BTree b -> b
+findMax t = foldr max (root t) t
+findMin :: Ord b => BTree b -> b
+findMin t = foldr min (root t) t
+
 instance Functor BTree where
-  fmap f (Leaf x) = Leaf (f x)
+  fmap f Empty = Empty
   fmap f (Node x y z) = Node (f x) (fmap f y) (fmap f z)
 
 instance Foldable BTree where
-  foldr f acc (Leaf x) = f x acc
+  foldr f acc Empty = acc
   foldr f acc (Node x y z) = foldr f (f x (foldr f acc z)) y
