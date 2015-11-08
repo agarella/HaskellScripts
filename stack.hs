bafbd0fcd4a8ac243e3394510f2bc8d6fe8695d2
Alex Garella <agarella@gmail.com>
2015-11-08 22:36:17 +0100

    Fix push type def and provide example

--- stack.hs
+++ stack.hs
@@ -6,8 +6,18 @@ import Control.Monad.State
 
 type Stack a = [a] 
 
-push :: a -> State (Stack) ()
+push :: a -> State (Stack a) ()
 push x = state $ \s -> ((), x : s)
 
 pop :: State (Stack a) a
 pop = state $ \(x : s) -> (x, s)
+
+-----
+-- Example
+----
+
+example = runState pushValues []
+  where pushValues = do _ <- push 1
+                        _ <- push 2
+                        z <- push 3
+                        return z
